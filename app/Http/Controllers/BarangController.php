bb54c83f72b795cbbd018d8b173262cadb8d6882
irmaulidia <irmamaulidiaaa@gmail.com>
2022-03-18 12:46:41 +0700

    barang & pelanggan

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\barang;
 
 class BarangController extends Controller
 {
     public function index()
     {
-        return view('barang', ['active'=>'active'], [
-            'title' => 'Barang',
-        ]);
+        $data = barang::all();
+        return view('barang', ['active'=>'active', 'title'=>'Barang'], compact('data'));
     }
 }
